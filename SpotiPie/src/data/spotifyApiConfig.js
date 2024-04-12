38780527cc596f8b583676e0ee114371e5ab71a6
ashzhang1 <ashzhang1@gmail.com>
2024-04-12 22:24:41 +1000

    added redirects file for netlify

--- SpotiPie/src/data/spotifyApiConfig.js
+++ SpotiPie/src/data/spotifyApiConfig.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 
 export const clientId = "670f5980caf649dfac1b4472838f14df";
-export const redirectUri = "http://localhost:5173/test";
+export const redirectUri = "https://spotipie.netlify.app/test"; //When testing, change to local host
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
