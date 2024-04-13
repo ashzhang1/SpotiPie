e9bcb29b335a6ea99268925ee8852cd52388f60e
ashzhang1 <ashzhang1@gmail.com>
2024-04-13 13:31:47 +1000

    added some comments

--- SpotiPie/src/data/spotifyApiConfig.js
+++ SpotiPie/src/data/spotifyApiConfig.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 
-export const clientId = "670f5980caf649dfac1b4472838f14df";
+export const clientId = "670f5980caf649dfac1b4472838f14df"; //If you want to run this on your own machine, you need to register the app on Spotify website and use your own clientId
 export const redirectUri = "https://spotipie.netlify.app/test"; //When testing, change to local host
 export const scopes = [
     "user-top-read",
